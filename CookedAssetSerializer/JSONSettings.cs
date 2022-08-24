5bcb993f2b838b522f75d4b6bbc461e760a64a9e
Narknon <narknon0@gmail.com>
2022-08-24 11:02:00 -0400

    begin adding nativization json settings

--- CookedAssetSerializer/JSONSettings.cs
+++ CookedAssetSerializer/JSONSettings.cs
@@ -7,15 +7,21 @@ public struct JSONSettings
     [JsonProperty] public string JSONDir;
     [JsonProperty] public string CookedDir;
     [JsonProperty] public string InfoDir;
+    [JsonProperty] public string CXXHdrDir;
+    [JsonProperty] public string DfltGamCfgDir;
     [JsonProperty] public UE4Version GlobalUEVersion;
+    [JsonProperty] public NativizedAssets.EBlueprintNativizationMethod nativMethod;
     [JsonProperty] public bool RefreshAssets;
     [JsonProperty] public bool DummyWithProps;
+    [JsonProperty] public bool UsesNative;
+    [JsonProperty] public bool GenNativEnumStruct;
     [JsonProperty] public List<EAssetType> SkipSerialization;
     [JsonProperty] public List<EAssetType> DummyAssets;
     [JsonProperty] public List<EAssetType> DeleteAssets;
     [JsonProperty] public List<string> CircularDependency;
     [JsonProperty] public List<string> SimpleAssets;
     [JsonProperty] public List<string> TypesToCopy;
+    [JsonProperty] public List<string> NativeBPList;
     [JsonProperty] public int SelectedIndex;
 
 }
