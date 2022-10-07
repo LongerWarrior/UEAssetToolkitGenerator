50f0fd8d41897d46be4d9940f7f3f0207b289252
Buckminsterfullerene02 <bobby45900@gmail.com>
2022-10-07 01:29:52 +0100

    Beginnings of fbx sdk interface

---
+++ FBXLib/Interface.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices;
+
+namespace FBXLib;
+
+public class Interface
+{
+    [DllImport("FBX_Wrapper.dll", CallingConvention=CallingConvention.StdCall, SetLastError = true, CharSet = CharSet.Ansi)]
+    public static extern void CreateFBXManager();
+    [DllImport("FBX_Wrapper.dll", CallingConvention=CallingConvention.StdCall, SetLastError = true, CharSet = CharSet.Ansi)]
+    public static extern void CreateFBXImporter();
+}
