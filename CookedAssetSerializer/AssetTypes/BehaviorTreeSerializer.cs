b698ae02b853979b35e817b1273f4a302ee31ccc
Buckminsterfullerene02 <bobby45900@gmail.com>
2023-09-09 16:28:56 +0100

    Blackboard & BehaviorTree serializers

---
+++ CookedAssetSerializer/AssetTypes/BehaviorTreeSerializer.cs
@@ -0,0 +1,10 @@
+﻿namespace CookedAssetSerializer.AssetTypes;
+
+public class BehaviorTreeSerializer : SimpleAssetSerializer<NormalExport>
+{
+    public BehaviorTreeSerializer(JSONSettings settings, UAsset asset) : base(settings, asset)
+    {
+        if (!Setup()) return;
+        SerializeAsset();
+    }
+}
